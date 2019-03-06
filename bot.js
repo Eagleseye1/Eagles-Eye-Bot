566bf81ab95e359ccac46f1b6ecb904f20660ad2
Eagleseye1 <48249240+Eagleseye1@users.noreply.github.com>
2019-03-06 15:54:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     
-    client.user.setGame ("Prefix ea!");
+    client.user.setGame ("prefix ea!");
 });
 
 const prefix = "ea!";
